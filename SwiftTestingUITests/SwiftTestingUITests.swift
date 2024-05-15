282d0f7bb034699b5d09a7d5e6b4487a1040aeeb
Meltem <meltemak8@gmail.com>
2024-05-15 16:37:11 +0200

    delete action is tested

--- SwiftTestingUITests/SwiftTestingUITests.swift
+++ SwiftTestingUITests/SwiftTestingUITests.swift
@@ -33,6 +33,36 @@ final class SwiftTestingUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
+    
+    func testDeleteItem() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let addButton = app.navigationBars["SwiftTesting.View"].buttons["Add"]
+        
+        let elementsQuery = app.alerts["Add Item"].scrollViews.otherElements
+        let textField = elementsQuery.collectionViews.textFields["Enter Item"]
+        
+        let okButton = elementsQuery.buttons["OK"]
+
+        let addedCell = app.tables.cells.containing(.staticText, identifier: "my to do").element
+        
+        //tables
+        let tablesQuery = app.tables
+        let deleteButton = tablesQuery.buttons["Delete"]
+        
+        addButton.tap()
+        textField.tap()
+        textField.typeText("my to do")
+        okButton.tap()
+        
+        addedCell.swipeLeft()
+        deleteButton.tap()
+        
+        XCTAssertFalse(addedCell.exists) // not line , added is ok
+        
+    }
+    
 
   
 }
